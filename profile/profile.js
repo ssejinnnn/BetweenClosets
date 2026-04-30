9a0f58319786f10987bf3ebcccfa7e3306a246c0
ssejinnnn <s7e.song@gmail.com>
2026-04-30 17:36:30 -0400

    Add files via upload

---
+++ profile/profile.js
@@ -0,0 +1,6 @@
+const editBtn = document.getElementById("editBtn");
+const editMessage = document.getElementById("editMessage");
+
+editBtn.addEventListener("click", function() {
+  editMessage.textContent = "Profile editing will open soon ♡";
+});
